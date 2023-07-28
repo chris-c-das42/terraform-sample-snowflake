cd07e2489834247ca339d29003fb009491a769af
Chris Cannon <c.cannon@das42.com>
2023-07-28 12:13:43 -0600

    Add Database and Warehouse

---
+++ main.tf
@@ -0,0 +1,27 @@
+/*
+In main.tf we set up the provider and define the configuration 
+for the database and the warehouse that we want Terraform to create.
+*/
+
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.68"
+    }
+  }
+}
+
+provider "snowflake" {
+  role = "SYSADMIN"
+}
+
+resource "snowflake_database" "db" {
+  name = "TF_DEMO"
+}
+
+resource "snowflake_warehouse" "warehouse" {
+  name           = "TF_DEMO"
+  warehouse_size = "large"
+  auto_suspend   = 60
+}
